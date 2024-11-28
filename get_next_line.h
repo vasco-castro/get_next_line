101ade13a223190e0149d51a4ea48746e2ed5215
Vasco Soares Sande E Castro <vsoares-@c2r3s5.42lisboa.com>
2024-11-28 18:59:41 +0000

    Use of mariocos cool buffshift function :) and clean code

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vsoares- <vsoares-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 17:13:57 by vsoares-          #+#    #+#             */
-/*   Updated: 2024/11/28 17:44:14 by vsoares-         ###   ########.fr       */
+/*   Updated: 2024/11/28 18:52:59 by vsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,8 @@
 # endif
 
 int		line_len(char *str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-// char	*salloc(size_t count);
-char	*memshift(char *dst, const char *src, size_t n);
+void	bufshift(char *buf, size_t size);
 char	*str_append(char *dst, char *src);
-
 char	*get_next_line(int fd);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: vsoares- <vsoares-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 17:13:54 by vsoares-          #+#    #+#             */
-/*   Updated: 2024/11/28 17:50:07 by vsoares-         ###   ########.fr       */
+/*   Updated: 2024/11/28 18:52:39 by vsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,57 +24,15 @@ int	line_len(char *str)
 	return (i);
 }
 
-void	ft_bzero(void *s, size_t n)
+void	bufshift(char *buf, size_t size)
 {
-	char	*src;
+	int	i;
 
-	src = (char *)s;
-	while (n-- > 0)
-		*(src++) = 0;
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void	*alloc;
-	size_t	total_size;
-
-	total_size = count * size;
-	if (count && ((total_size / count) != size))
-		return (NULL);
-	alloc = malloc(total_size);
-	if (!alloc)
-		return (NULL);
-	ft_bzero(alloc, total_size);
-	return (alloc);
-}
-
-// char	*salloc(size_t count)
-// {
-// 	char	*alloc;
-// 	size_t	i;
-
-// 	alloc = malloc(count);
-// 	if (!alloc)
-// 		return (NULL);
-// 	i = count;
-// 	while (i-- > 0)
-// 		*(alloc++) = '\0';
-// 	return (alloc);
-// }
-
-char	*memshift(char *dst, const char *src, size_t n)
-{
-	char	*tmp_dst;
-	char	*tmp_src;
-
-	if (!src && !dst)
-		return (NULL);
-	tmp_dst = (char *) dst;
-	tmp_src = (char *) src;
-	while (n-- > 0)
-		*(tmp_dst++) = *(tmp_src++);
-	*tmp_dst = 0;
-	return (dst);
+	i = 0;
+	while (buf[size])
+		buf[i++] = buf[size++];
+	while (i < BUFFER_SIZE)
+		buf[i++] = 0;
 }
 
 char	*str_append(char *old_line, char *buf)
@@ -85,7 +43,7 @@ char	*str_append(char *old_line, char *buf)
 
 	i = 0;
 	j = 0;
-	new_line = ft_calloc(line_len(old_line) + line_len(buf) + 2, 1);
+	new_line = malloc(line_len(old_line) + line_len(buf) + 2);
 	if (!new_line)
 		return (NULL);
 	while (old_line && old_line[i])
@@ -93,12 +51,10 @@ char	*str_append(char *old_line, char *buf)
 		new_line[i] = old_line[i];
 		i++;
 	}
-	while (buf[j] && buf[j] != '\n')
+	while (buf[j] && buf[j - 1] != '\n')
 		new_line[i++] = buf[j++];
-	if (buf[j] == '\n')
-		new_line[i++] = '\n';
-	new_line[i] = '\0';
-	buf = memshift(buf, buf + j + 1, BUFFER_SIZE - j);
+	new_line[i] = 0;
+	bufshift(buf, j);
 	free(old_line);
 	return (new_line);
 }
